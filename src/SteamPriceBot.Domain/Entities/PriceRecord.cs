ec303dec124f366a8f16bec2f9d6c1a5b6fc9182
avisqqq <vasylambroz@gmail.com>
2025-11-03 20:59:25 +0100

    Fix: PriceRecord Add Guid TrackedItemId for better db layout And ctor for EFCore

--- src/SteamPriceBot.Domain/Entities/PriceRecord.cs
+++ src/SteamPriceBot.Domain/Entities/PriceRecord.cs
@@ -2,17 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SteamPriceBot.Domain.Common;
 using SteamPriceBot.Domain.ValueObjects;
 
 namespace SteamPriceBot.Domain.Entities
 {
-    public class PriceRecord
+    public class PriceRecord : EntityBase
     {
+        public Guid TrackedItemId { get; private set; }
         public DateTime TimestampUtc { get; init; } = DateTime.UtcNow;
         public PriceValue Price { get; init; }
-        public PriceRecord(PriceValue price)
+        public PriceRecord(Guid trackedItemId,PriceValue price)
         {
+            TrackedItemId = trackedItemId;
             Price = price;
         }
+        // EF COre private constructor
+        private PriceRecord(){}
     }
 }
