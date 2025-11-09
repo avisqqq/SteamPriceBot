ead728554820603914b946468933fc1f24231054
avisqqq <vasylambroz@gmail.com>
2025-11-09 21:04:09 +0100

    Fix: add services configuration To test local web

--- src/SteamPriceBot.Api/Program.cs
+++ src/SteamPriceBot.Api/Program.cs
@@ -6,11 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+// For local testing
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowAll"); // Local tests
 
 if (app.Environment.IsDevelopment())
 {
